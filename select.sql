59cccf349fc251c00173806737bc7cfec70dc6db
Joshua "Moose" Archer <joshua.archer@techtonic.com>
2019-05-28 08:33:23 -0600

    Two queries using the select clause on the meals and inventory tables

--- select.sql
+++ select.sql
@@ -1,1 +1,6 @@
+-- Select everything from the meals table
+SELECT * FROM chez_moose.meals;
 
+-- Selecting specific rows from the inventory table
+SELECT item_id, item_name, number_of_units
+FROM chez_moose.inventory;

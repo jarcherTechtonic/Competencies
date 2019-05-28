0be0692471c35d7d5839d2cc53d2ad2116ff5beb
Joshua "Moose" Archer <joshua.archer@techtonic.com>
2019-05-28 09:51:17 -0600

    deleted row from drinks table

--- delete.sql
+++ delete.sql
@@ -1,2 +1,7 @@
 -- Let's say that we enter in some unwanted data by mistake
-INSERT INTO chez_moose.drinks
+INSERT INTO chez_moose.drinks (drink_id, drink_type, drink_name, expires, number_of_units) 
+VALUES ("10", "alcohol", "Not our Restaurant's IPA", "2009-03-01", "30");
+
+-- Since that's not our restaurant's beer, let's delete it from our table!
+DELETE FROM chez_moose.drinks
+WHERE drink_id = "10";
